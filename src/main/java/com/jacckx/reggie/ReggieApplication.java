002d8b13eadbc7b578f27f523ef73723abe3df8e
Jacckx <68081752+Jacckx@users.noreply.github.com>
2023-04-24 18:09:51 +0100

    添加登入拦截器

--- src/main/java/com/jacckx/reggie/ReggieApplication.java
+++ src/main/java/com/jacckx/reggie/ReggieApplication.java
@@ -3,15 +3,12 @@ package com.jacckx.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.*;
 
-/**
- * @author Shaojun Chen
- * @version 1.0
- * @since 2023/4/24 1:48
- */
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class, args);
