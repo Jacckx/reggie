8722f88bf3cad0ed648b4f78a3e6a3fd90e00d12
Jacckx <68081752+Jacckx@users.noreply.github.com>
2023-04-24 02:00:18 +0100

    添加前端静态资源，项目启动成功

---
+++ src/main/java/com/jacckx/reggie/ReggieApplication.java
@@ -0,0 +1,20 @@
+package com.jacckx.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Shaojun Chen
+ * @version 1.0
+ * @since 2023/4/24 1:48
+ */
+
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+        log.info("项目启动成功...");
+    }
+}
---
+++ src/main/java/com/jacckx/reggie/config/WebMvcConfig.java
@@ -0,0 +1,29 @@
+package com.jacckx.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author Shaojun Chen
+ * @version 1.0
+ * @since 2023/4/24 1:53
+ */
+
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始进行静态资源映射...");
+        // 把浏览器的请求映射到backend目录下
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
